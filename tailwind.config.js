a40aa18f291fa23d8b3aa9aa4469e88a0a029c38
allanHollar <allan@hollar.com>
2025-03-17 16:37:53 -0700

    Fix build css and add watch. Added hero image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,36 @@
-const colors = require('tailwindcss/colors');
+/** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-	content: ['./src/**/*.{js,jsx,ts,tsx}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				'primary-light': '#F7F8FC',
-				'secondary-light': '#FFFFFF',
-				'ternary-light': '#f6f7f8',
-
-				'primary-dark': '#0D2438',
-				'secondary-dark': '#102D44',
-				'ternary-dark': '#1E3851',
-			},
-			container: {
-				padding: {
-					DEFAULT: '1rem',
-					sm: '2rem',
-					lg: '5rem',
-					xl: '6rem',
-					'2xl': '8rem',
-				},
-			},
-		},
-	},
-	variants: {
-		extend: { opacity: ['disabled'] },
-	},
-	plugins: ['@tailwindcss/forms'],
+  content: [
+    "./index.js",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class", // Enables dark mode support
+  theme: {
+    extend: {
+      colors: {
+        "primary-light": "#F7F8FC",
+        "secondary-light": "#FFFFFF",
+        "ternary-light": "#f6f7f8",
+        "primary-dark": "#0D2438",
+        "secondary-dark": "#102D44",
+        "ternary-dark": "#1E3851",
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "5rem",
+          xl: "6rem",
+          "2xl": "8rem",
+        },
+      },
+      opacity: ["disabled"], // Moved from variants to extend
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
 };
