f138b780b3a223ead552ad99c0093623683511cf
allanHollar <allan@hollar.com>
2025-03-30 22:33:29 -0700

    Remove extra data file and data import

--- src/context/AboutMeContext.jsx
+++ src/context/AboutMeContext.jsx
@@ -1,31 +1,22 @@
-import { useState, createContext } from 'react';
+import { useState, createContext } from "react";
 
-import { aboutMeData } from '../data/aboutMeData';
-import { clientsHeading as clientsPageHeading } from '../data/clientsData';
-import { clientsData as clientsDataJson } from '../data/clientsData';
+import { aboutMeData } from "../data/aboutMeData";
 
 const AboutMeContext = createContext();
 
 export const AboutMeProvider = ({ children }) => {
-	const [aboutMe, setAboutMe] = useState(aboutMeData);
+  const [aboutMe, setAboutMe] = useState(aboutMeData);
 
-	const clientsHeading = clientsPageHeading;
-
-	const [clientsData, setClientsData] = useState(clientsDataJson);
-
-	return (
-		<AboutMeContext.Provider
-			value={{
-				aboutMe,
-				setAboutMe,
-				clientsHeading,
-				clientsData,
-				setClientsData,
-			}}
-		>
-			{children}
-		</AboutMeContext.Provider>
-	);
+  return (
+    <AboutMeContext.Provider
+      value={{
+        aboutMe,
+        setAboutMe,
+      }}
+    >
+      {children}
+    </AboutMeContext.Provider>
+  );
 };
 
 export default AboutMeContext;
