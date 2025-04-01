d05dcd44ae3c2daa6b4ff8b7a47d3db4c82198e6
allanHollar <allan@hollar.com>
2025-03-31 21:01:17 -0700

    Reduce the coffee cup size

--- src/components/reusable/CoffeeCup/CoffeeCup.jsx
+++ src/components/reusable/CoffeeCup/CoffeeCup.jsx
@@ -2,7 +2,7 @@ import styles from "./Coffee.module.scss";
 
 const CoffeeCup = () => {
   return (
-    <div className={` mt-10 mb-5 sm:mt-24 ${styles.cup}`} id="coffee">
+    <div className={` mt-10 mb-5 sm:mt-12 ${styles.cup}`} id="coffee">
       <div className={styles.coffee}></div>
     </div>
   );
