5726b6d7e6f70377d0d2cc9e1eb0dc96483894e2
allanHollar <allan@hollar.com>
2025-04-07 22:27:10 -0700

    Create a cssStules prop

--- src/components/shared/AppBanner/AppBanner.tsx
+++ src/components/shared/AppBanner/AppBanner.tsx
@@ -1,5 +1,6 @@
-import { motion } from "framer-motion";
 import { useMemo } from "react";
+import { motion } from "framer-motion";
+import { PageHeading, BlurIn } from "../../reusable/TextAnimation";
 
 import "./fireflies.sass";
 
@@ -19,32 +20,18 @@ const AppBanner = () => {
       className="relative flex sm:flex-row flex-col justify-center sm:justify-between items-center bg-hero-image-mobile sm:bg-hero-image bg-cover bg-no-repeat bg-center h-[550px] sm:h-[750px] overflow-hidden"
     >
       <div className="flex flex-col justify-center items-center w-full">
-        <motion.h1
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            ease: "easeInOut",
-            duration: 0.9,
-            delay: 0.1,
-          }}
-          className="drop-shadow-md mt-[-30px] sm:mt-[-100px] text-5xl text-center text-white md:text-7xl happy-monkey-regular"
-          style={{ textShadow: "rgba(0, 0, 0, 0.75) .05em .05em 4px" }}
-        >
-          Hello, I'm Allan
-        </motion.h1>
-        <motion.p
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            ease: "easeInOut",
-            duration: 0.9,
-            delay: 0.2,
-          }}
-          className="mt-3 text-2xl text-center text-white sm:text-4xl dark:text-gray-200 leading-normal happy-monkey-regular"
-          style={{ textShadow: "rgba(0, 0, 0, 0.75) .05em .05em 2px" }}
+        <h1 className="hidden">Hello, I'm Allan</h1>
+        <PageHeading
+          text="Hello, I'm Allan"
+          cssStyles="drop-shadow-md mt-[-30px] sm:mt-[-100px] text-5xl text-center text-white md:text-7xl happy-monkey-regular"
+        />
+        <BlurIn
+          cssStyles={
+            "sm:mt-[-25px] text-2xl text-center text-white sm:text-4xl dark:text-gray-200 leading-normal happy-monkey-regular"
+          }
         >
           Senior Frontend Engineer & UX/UI Enthusiast!
-        </motion.p>
+        </BlurIn>
       </div>
       {fireflies}
     </motion.section>
